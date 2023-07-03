47633a19596998a130987ca02db9753d046bd299
amirxghd <amirreza.ghaffari.d@gmail.com>
2023-07-03 09:35:58 +0330

    Update(): add class based view

--- crypto/api/v1/serializers.py
+++ crypto/api/v1/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
-from ...models import CryptoTransaction, CryptoCurrency
+from ...models import (
+    CryptoTransaction,
+    CryptoCurrency
+)
+from users.models import CustomUser
+from django.shortcuts import get_object_or_404
+from ...tools import buy_from_exchange
 
 
 class CryptoCurrencySerializer(serializers.ModelSerializer):
@@ -8,12 +14,21 @@ class CryptoCurrencySerializer(serializers.ModelSerializer):
         fields = ['name']
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('full_name', 'id', )
+
+
 class TransactionSerializer(serializers.ModelSerializer):
-    coin = CryptoCurrencySerializer(many=False, read_only=True)
+    user = UserSerializer(read_only=True)
+    coin_name = serializers.CharField(source='coin.name')
+
     class Meta:
         model = CryptoTransaction
-        fields = ['user', 'coin', 'amount']
-
+        fields = ['user', 'amount', 'coin_name']
+        read_only_fields = ['user']
+        depth = 1
 
 
 
--- crypto/api/v1/views.py
+++ crypto/api/v1/views.py
@@ -1,25 +1,30 @@
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.decorators import (
+    api_view,
+    authentication_classes,
+    permission_classes
+)
+from rest_framework.authentication import (
+    SessionAuthentication,
+    BasicAuthentication
+)
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework import viewsets, status
-from ...models import CryptoCurrency, Wallet, CryptoTransaction
+from rest_framework import (
+    viewsets,
+    status
+)
+from ...models import (
+    CryptoCurrency,
+    CryptoTransaction
+)
 from .serializers import TransactionSerializer
 from django.shortcuts import get_object_or_404
 from ...tools import buy_from_exchange
 from django.db import transaction
-from users.models import CustomUser, TomanWallet
+from users.models import TomanWallet
 
 
-class TransactionViewSet(viewsets.ViewSet):
-    """
-    A simple ViewSet for listing or retrieving Transactions.
-    """
-    def list(self, request):
-        queryset = CryptoTransaction.objects.all()
-        serializer = TransactionSerializer(queryset, many=True)
-        return Response(serializer.data)
-
+# ------ Functional Api view ------
 
 @api_view(['POST'])
 @authentication_classes([SessionAuthentication, BasicAuthentication])
@@ -54,6 +59,47 @@ def do_transaction(request):
             return Response({"message": "something went wrong"}, status=status.HTTP_400_BAD_REQUEST)
 
     return Response({"message": "amount or coin is missing"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+# ------ Class based Api view ------
+
+
+class TransactionViewSet(viewsets.ViewSet):
+
+    """
+    A simple ViewSet for listing or creating Transactions.
+    """
+
+    serializer_class = TransactionSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+
+    def list(self, request):
+        queryset = CryptoTransaction.objects.all()
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        coin = get_object_or_404(CryptoCurrency, name=serializer.validated_data['coin']['name'])
+        amount = serializer.validated_data['amount']
+        with transaction.atomic():
+
+            user_wallet = TomanWallet.objects.select_for_update().get(user=request.user)
+            user_wallet.balance -= amount
+            user_wallet.save()
+
+            if amount >= 10:
+                serializer.validated_data['is_settlement'] = True
+                buy_from_exchange(crypto_name=coin.name, amount=amount)
+            serializer.save(user=request.user, coin=coin)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+
 
 
 
